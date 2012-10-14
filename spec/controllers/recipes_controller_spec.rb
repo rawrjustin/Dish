e5011c2606b282730c7a8542e820a611e228bde2
Mike <mike@mike-PORTEGE-Z835.(none)>
2012-10-14 16:23:51 -0700

    added rspec tests for adding a new recipe

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe RecipesController do
+
+  describe 'new' do
+    it 'should render the view with the new recipe form' do
+      get 'new'
+      response.should render_template('new')
+    end
+  end
+
+  describe 'create' do
+    it 'should call the model to create a new recipe in the database' do
+      recipe = mock('recipe')
+      Recipe.should_receive(:create).and_return(recipe)
+      get 'create', {:recipe => recipe}
+    end
+    it 'should redirect to recipe details page upon submission' do
+      recipe = mock('recipe')
+      Recipe.stub(:create).and_return(recipe)
+      get 'create', {:recipe => recipe}
+      response.should redirect_to('show')
+    end
+  end
+
 end
