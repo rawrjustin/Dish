b65989ed2855f0a496d30439a3733e1edda83cda
Gideon <gideon@timemachine.(none)>
2012-10-21 17:35:17 -0700

    did some better sentence structure:

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,5 +7,27 @@ class Recipe < ActiveRecord::Base
     if servings > 0
       cost_per_person = self.total_cost.to_f / self.servings.to_f
     end
+    return cost_per_person.to_f
+  end
+
+  def time
+    hours = self.time_in_minutes.to_i / 60
+    mins = self.time_in_minutes.to_i - hours * 60
+    total_time = ''
+    if hours > 0
+      if hours == 1
+        total_time += "1 hour "
+      else
+        total_time += "#{hours} hours "
+      end
+    end
+    if mins > 0
+      if mins == 1
+        total_time += "1 min"
+      else
+        total_time += "${mins} mins"
+      end
+    end
+    return total_time
   end
 end
