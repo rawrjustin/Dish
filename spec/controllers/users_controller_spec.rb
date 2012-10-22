fc820beb659b1579aefa5e00fe42c30eb696e38b
Mike <mike@mike-PORTEGE-Z835.(none)>
2012-10-21 17:55:03 -0700

    added couple spec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,17 +8,31 @@ describe UsersController do
   end
 
   describe "GET 'show'" do
-    
+
     it "should be successful" do
       get :show, :id => @user.id
       response.should be_success
     end
-    
+
     it "should find the right user" do
       get :show, :id => @user.id
       assigns(:user).should == @user
     end
-    
+
+  end
+
+  describe "GET 'index'" do
+
+    it "should be successful" do
+      get :index
+      response.should be_success
+    end
+
+    it "should see the user" do
+      User.should_receive(:all).and_return(@user)
+      get :index
+    end
+
   end
 
 end
