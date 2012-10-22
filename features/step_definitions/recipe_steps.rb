dde9f8dff6413913e7f04eb0b8c9416e2bd17a8b
Brent Batas <brentbatas@gmail.com>
2012-10-21 17:33:56 -0700

    Fixed recipe steps to be compatible with validations

--- features/step_definitions/recipe_steps.rb
+++ features/step_definitions/recipe_steps.rb
@@ -9,6 +9,12 @@ Given /^these recipes exist: "([^"]*)"$/ do |recipes|
   recipes_list.each do |recipe_name|
     r = Recipe.create
     r[:name] = recipe_name
+    r[:description] = "..."
+    r[:ingredients] = "..."
+    r[:directions] = "..."
+    r[:time_in_minutes] = 5
+    r[:servings] = 5
+    r[:total_cost] = 5
     r.save
   end
 end
@@ -29,5 +35,11 @@ end
 Given /^I add a recipe for "(.*?)"$/ do |recipe_name|
   visit '/recipes/new'
   fill_in "recipe_name", :with => recipe_name
+  fill_in "recipe_description", :with => "..."
+  fill_in "recipe_ingredients", :with => "..."
+  fill_in "recipe_directions", :with => "..."
+  fill_in "recipe_time_in_minutes", :with => 5
+  fill_in "recipe_servings", :with => 5
+  fill_in "recipe_total_cost", :with => 5
   click_button "Save Changes"
 end
