d906b1d314cbca45683a5c395251d553986c1764
Gideon <gideon@timemachine.(none)>
2012-10-27 23:02:25 -0700

    schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121014013359) do
+ActiveRecord::Schema.define(:version => 20121028053549) do
+
+  create_table "caterers", :force => true do |t|
+    t.integer  "phone"
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "recipes", :force => true do |t|
     t.string   "name"
