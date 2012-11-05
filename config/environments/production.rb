ee250b0c8b6dbb7768f549fdbed1e0fd75a3ccd3
Mike <mike@mike-PORTEGE-Z835.(none)>
2012-11-05 13:27:56 -0800

    Heroku fix 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ FoodPrepApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   #heroku fix for active admin
-  config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.assets.precompile += %w[active_admin.css active_admin.js active_admin/print.css]
 
   # Code is not reloaded between requests
   config.cache_classes = true
