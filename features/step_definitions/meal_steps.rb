6b2e07b7ebaaaa6f9fbd3f1bf2a97344cb814ed4
Mike <mike@mike-PORTEGE-Z835.(none)>
2012-11-04 18:05:10 -0800

    renamed feature file names

--- features/step_definitions/meal_steps.rb
+++ features/step_definitions/meal_steps.rb
@@ -13,3 +13,26 @@ When /^I fill in fields for cooked meal "(.*?)"$/ do |name|
   fill_in "Time in minutes", :with => 30
   fill_in "Servings", :with => 200
 end
+
+Given /^the following meals exist:?$/ do |table|
+  table.hashes.each do |hash|
+    FactoryGirl.create(:meal, hash)
+  end
+end
+
+Given /^these meals exist: "([^"]*)"$/ do |meals|
+  meals_list = meals.split(',')
+  meals_list.each do |meal_name|
+    m = Meal.create
+    m[:name] = meal_name
+    m[:description] = "..."
+    m[:phone] = "0123456789"
+    m[:address] = "..."
+    m[:ingredients] = "..."
+    m[:servings] = 100
+    m[:directions] = "..."
+    m[:time_in_minutes] = 5
+    m[:total_cost] = 5
+    m.save!
+  end
+end
