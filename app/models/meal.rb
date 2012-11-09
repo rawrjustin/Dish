ad5bcfbba24f810afa05c821f02573750ff3c78b
Mike <mike@mike-PORTEGE-Z835.(none)>
2012-11-08 21:39:10 -0800

    made 'thumb' a accessible attribute, fixed bug with admin panel adding recipes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,6 @@
 class Meal < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :name, :description, :servings
-  validates :name, :description, :servings, :presence => true
+  attr_accessible :name, :description, :servings, :thumb
+  validates :name, :description, :servings, :thumb, :presence => true
   validates_numericality_of :servings
 end
