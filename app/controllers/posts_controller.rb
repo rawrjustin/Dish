b27dba6818dbd781116c233b4664d5b5f85d69c8
Gideon <gideon@timemachine.(none)>
2012-11-26 13:08:07 -0800

    a kind of buggy search interface for blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.find(:all, :order => 'created_at desc')
+    search_term = ""
+    if params["term"]
+      search_term = params["term"]
+    end
+    title_results = Post.search({"title_cont" => search_term}).result(:distinct => true, :order => 'created_at desc')
+    content_results = Post.search({"content_cont" => search_term}).result(:distinct => true, :order => 'created_at desc')
+    blah = title_results.merge(content_results)
+    if blah.count > 0
+      @posts = blah
+    else
+      @posts = Post.find(:all, :order => 'created_at desc')
+    end
   end
 
   def show
