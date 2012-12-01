e91daaeb9143df0e1744a864bcf94a5eb4c6822d
Gideon <gideon@timemachine.(none)>
2012-12-01 14:06:01 -0800

    implemented ingredients and location, ingredients can choose location

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ActiveRecord::Base
-  attr_accessible :location, :name, :notes, :photo, :price
+  attr_accessible :location, :name, :notes, :photo, :price, :location_id
   belongs_to :location
+  validates :name, :price, :location, :presence => true
+  validates_numericality_of :price, :on => :create, :greater_than_or_equal_to => 0
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,8 @@ class Location < ActiveRecord::Base
   attr_accessible :address, :city, :name
   has_many :ingredients
   validates :name, :city, :presence => true
+
+  def display_name
+    self.name + " (" + self.city + ")"
+  end
 end
