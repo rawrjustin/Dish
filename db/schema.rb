c0646e5c362b1f8f69b37c6a04a68a9532e2599b
Mike <mike@mike-PORTEGE-Z835.(none)>
2012-12-04 17:09:09 -0800

    Changed db for gideon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121204214907) do
+ActiveRecord::Schema.define(:version => 20121204231736) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(:version => 20121204214907) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "thumb"
+    t.string   "cost"
   end
 
   create_table "posts", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,41 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed (or create!d alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 seed_file = File.join(Rails.root, 'db', 'seed.yml')
 config = YAML::load_file(seed_file)
-User.create(config["users"])
-Post.create(config["posts"])
-Location.create(config["locations"])
-config["ingredients"].each do |ingredient|
-  location_name = ingredient["location"]
-  ingredient["location"] = Location.find_by_name(location_name)
-  Ingredient.create(ingredient)
-end
-config["recipes"].each do |recipe|
-  ingredient_list = recipe.delete("ingredients")
-  rec = Recipe.create(recipe)
-  if ingredient_list
-    ingredient_list.each do |ingredient|
-      ing = Ingredient.find_by_name(ingredient["name"])
-      RecipeIngredient.create do |r|
-        r.recipe = rec
-        r.ingredient = ing
-        r.amount = ingredient["size"]
-      end
-    end
-  end
-end
+#User.create!(config["users"])
+#Post.create!(config["posts"])
+#Location.create!(config["locations"])
+#config["ingredients"].each do |ingredient|
+  #location_name = ingredient["location"]
+  #ingredient["location"] = Location.find_by_name(location_name)
+  #Ingredient.create!(ingredient)
+#end
+#config["recipes"].each do |recipe|
+  #ingredient_list = recipe.delete("ingredients")
+  #rec = Recipe.create!(recipe)
+  #if ingredient_list
+    #ingredient_list.each do |ingredient|
+      #ing = Ingredient.find_by_name(ingredient["name"])
+      #RecipeIngredient.create! do |r|
+        #r.recipe = rec
+        #r.ingredient = ing
+        #r.amount = ingredient["size"]
+      #end
+    #end
+  #end
+#end
 config["cooked_meals"].each do |meal|
   ingredient_list = meal.delete("ingredients")
-  m = CookedMeal.create(meal)
+  m = CookedMeal.create!(meal)
   if ingredient_list
     ingredient_list.each do |ingredient|
       ing = Ingredient.find_by_name(ingredient["name"])
-      CookedMealIngredient.create do |c|
+      CookedMealIngredient.create! do |c|
         c.cooked_meal = m
         c.ingredient = ing
         c.amount = ingredient["size"]
@@ -43,4 +43,4 @@ config["cooked_meals"].each do |meal|
     end
   end
 end
-CateredMeal.create(config["catered_meals"])
+CateredMeal.create!(config["catered_meals"])
